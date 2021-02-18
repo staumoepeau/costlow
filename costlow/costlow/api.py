1aa491ec8ece142742b139393f7e20fa055095ee
staumoepeau <sione.taumoepeau@gmail.com>
2021-02-18 16:38:15 +1300

    Update POS

--- costlow/costlow/api.py
+++ costlow/costlow/api.py
@@ -15,6 +15,31 @@ from pymysql.constants.ER import DATA_TOO_LONG
 from psycopg2.errorcodes import STRING_DATA_RIGHT_TRUNCATION
 
 
+class LegacyPassword(pbkdf2_sha256):
+	name = "frappe_legacy"
+	ident = "$frappel$"
+
+	def _calc_checksum(self, secret):
+		# check if this is a mysql hash
+		# it is possible that we will generate a false positive if the users password happens to be 40 hex chars proceeded
+		# by an * char, but this seems highly unlikely
+		if not (secret[0] == "*" and len(secret) == 41 and all(c in string.hexdigits for c in secret[1:])):
+			secret = mysql41.hash(secret + self.salt.decode('utf-8'))
+		return super(LegacyPassword, self)._calc_checksum(secret)
+
+
+register_crypt_handler(LegacyPassword, force=True)
+passlibctx = CryptContext(
+	schemes=[
+		"pbkdf2_sha256",
+		"argon2",
+		"frappe_legacy",
+	],
+	deprecated=[
+		"frappe_legacy",
+	],
+)
+
 @frappe.whitelist(allow_guest=True)
 def check_password(user, pwd, doctype='User', fieldname='password'):
 	'''Checks if user and password are correct, else raises frappe.AuthenticationError'''
@@ -24,11 +49,13 @@ def check_password(user, pwd, doctype='User', fieldname='password'):
 		{'doctype': doctype, 'name': user, 'fieldname': fieldname}, as_dict=True)
 
 	if not auth or not passlibctx.verify(pwd, auth[0].password):
-		raise frappe.AuthenticationError(_('Incorrect User or Password'))
+		frappe.msgprint("Incorrect User or Password", raise_exception=True)
+
+#		raise frappe.AuthenticationError(_('Incorrect User or Password'))
 
 	# lettercase agnostic
-	user = auth[0].name
-	delete_login_failed_cache(user)
+#	user = auth[0].name
+#	delete_login_failed_cache(user)
 
 	return user
 # searches for leads which are not converted
